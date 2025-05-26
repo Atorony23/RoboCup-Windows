946f44ea44cb020f18d4d308f89bf05ee09632e5
Atorony23 <164085114+Atorony23@users.noreply.github.com>
2025-05-26 16:35:26 -0600

    Add files via upload

--- cesto.m
+++ cesto.m
@@ -1,18 +1,11 @@
 function cesto_debasura = cesto(objeto)
-    switch objeto
-        case "Lata" | "SPAM" | "Verde" | "Rosa"
-            % Bote verde
-           cesto_debasura = 1;
-
-        case "Botella" | "Tapa" | "Plumon" | "Rojo" | "Azul"
-            % Bote azul
-           cesto_debasura = 0;
+    if ismember(objeto, ["Lata", "SPAM", "Verde", "Rosa"])
+        % Bote verde
+        cesto_debasura = 1;
+    elseif ismember(objeto, ["Botella", "Tapa", "Plumon", "Rojo", "Azul"])
+        % Bote azul
+        cesto_debasura = 0;
+    else
+        error('Objeto no reconocido');
     end
 end
-
-%0--Esta funcion nos servira para poder definir y seleccionar de manera
-%automatica el valor del cesto, si queremos que sea cesto verde o azul
-
-%1--Utilizamos un switch case que nos definira si es lata o spam o verde o
-%rosa el objeto nos devolvera el cesto 1 (verde) o si es alguno de los
-%otros objetos nos devolvera en el cesto 0 (azul)
