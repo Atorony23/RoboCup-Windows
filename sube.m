d893725b46aa40a3ac5bd40db0e973267cd4cb1a
Atorony23 <164085114+Atorony23@users.noreply.github.com>
2025-05-30 16:25:21 -0600

    Add files via upload

--- sube.m
+++ sube.m
@@ -1,6 +1,6 @@
 function jointAngles = sube(coci,jointAngles,defase_Robot,ur)
     %Hara que suba sobre el mismo punto
-    h_deseada = 0.25;
+    h_deseada = 0.30;
     h_posible = min(h_deseada,sqrt((1-(coci(1)/0.91)^2)*0.6^2));
     coci = [coci(1),coci(2),h_posible];
     jointAngles = MoverRobot(coci,0,defase_Robot,jointAngles,ur);
